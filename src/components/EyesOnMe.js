578bb4fb0809a07b4d25146c5ceb902b2b89e2c9
Anne Muriuki <muriukianne05@gmail.com>
2024-11-14 12:54:32 +0300

    add eventhandler

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,18 @@
 // Code EyesOnMe Component Here
+import React from "react";
+
+export default function EyesOnMe() {
+  function onFocus() {
+    console.log('Good!')
+  }
+
+  function onBlur() {
+    console.log('Hey! Eyes on me!')
+  }
+
+  return (
+    <div>
+      <button onFocus={onFocus} onBlur={onBlur}>Eyes on me</button>
+    </div>
+  );
+}
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,9 +1,22 @@
 // Code Keypad Component Here
 
+
 function Keypad (){
-    return (
-        <div></div>
-    )
+    function handleChange(event) {
+        console.log(`${event.target.name}`);
+      }
+    
+      return (
+        <div>
+          <input
+            type="password"
+            name="Entering password..."
+            onChange={handleChange}
+            placeholder="Enter password"
+          />
+        </div>
+      );
+    
 }
 
 export default Keypad;
